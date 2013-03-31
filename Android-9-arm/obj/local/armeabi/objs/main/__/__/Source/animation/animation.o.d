85e63abd220edf03e98039d5ea34b96780adf795
Peter Drayman <ichbinpunk@gmail.com>
2013-03-31 03:32:01 +0200

    .gitignore activated

--- Android-9-arm/obj/local/armeabi/objs/main/__/__/Source/animation/animation.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/main/__/__/Source/animation/animation.o: \
- jni/../../Source/animation/animation.cpp \
- jni/../../Source/animation/animation.h
-
-jni/../../Source/animation/animation.h:
--- Android-9-arm/obj/local/armeabi/objs/main/__/__/Source/game/key_map.o.d
+++
@@ -1,4 +0,0 @@
-obj/local/armeabi/objs/main/__/__/Source/game/key_map.o: \
- jni/../../Source/game/key_map.cpp jni/../../Source/game/key_map.h
-
-jni/../../Source/game/key_map.h:
